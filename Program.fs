086ee6314cf3b6aaad71cdc2bf2b32b36a4726e4
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2022-07-30 00:50:47 +0300

    feat: sentence output

--- Program.fs
+++ Program.fs
@@ -1,2 +1,9 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+let sentences = [ ["#START"; "I"; "swim"; "."; "#END"]; ["#START"; "I"; "go"; "swimming"; "."; "#END"] ] ;
+
+let sentenceOutput(sentence: List<string>) = String.concat " " sentence
+
+
+for sentence in sentences do
+    printfn "%s \n" (sentenceOutput(sentence))
