eba0cf140cfb1e542c23cb72225225afe30bd8a9
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2022-07-31 15:27:08 +0300

    feat: get chain from corpus

--- Program.fs
+++ Program.fs
@@ -1,10 +1,12 @@
 ﻿// TODO: Markov Chain Sentence Generator
 
 open System;
+open System.Linq;
+
 
 let sentences = [ ["#START"; "I"; "swim"; "."]; ["#START"; "I"; "go"; "swimming"; "."] ] ;
 
-let sentenceOutput(sentence: List<string>) =
+let SentenceOutput(sentence: List<string>) =
     let mutable output = "";
     for token in sentence do
         match token with
@@ -13,18 +15,50 @@ let sentenceOutput(sentence: List<string>) =
         | _ -> output <- if String.Equals("", output) then token else String.concat " " [output; token]
     output
 
-let getNGram (tokenPos: int, n: int, sentence: List<string>) =
+let GetNGram (tokenPos: int, n: int, sentence: List<string>) =
     let mutable output : List<string> = [];
     for i = tokenPos to tokenPos + n - 1 do
         if i < sentence.Length then output <- output @ [sentence[i]] else output <- output @ ["#END"];
     output
 
 
-let splitSentenceIntoNGrams (sentence: List<string>, n: int) =
+let CollectSubchains (sentences: List<List<string>>, n: int) =
     let mutable output : List<List<string>> = [];
-    for i = 0 to sentence.Length - 1 do
-        output <- output @ [getNGram(i, n, sentence)];
+    for sentence in sentences do
+        for i = 0 to sentence.Length - 1 do
+            output <- output @ [GetNGram(i, n, sentence)];
     output
 
-for ngram in splitSentenceIntoNGrams(sentences[0], 2) do
-    printfn "%s" (String.concat " " ngram)
+type Chained =
+    {mutable value: List<string>;
+    mutable amount: int;
+    mutable probability: float;}
+
+type Link =
+    {mutable joined: List<Chained>;}
+    member this.CalculateProbabilities() =
+        let overallAmount  = (float) (List.reduce(fun x y -> x + y) (List.map (fun x -> x.value.Length) this.joined));
+        for sequence in this.joined do
+            sequence.probability <- (float) sequence.value.Length / overallAmount;
+
+
+let getChain(collectedSubchains: List<List<string>>) =
+    let mutable chain  = new Collections.Generic.Dictionary<string, Link>();
+    for subchain in collectedSubchains do
+        let sequitur = [for i = 1 to subchain.Length - 1 do subchain[i]];
+        if (chain.Keys.Contains(subchain[0])) then
+            if List.exists(fun x -> List.forall2(fun elem1 elem2 -> String.Equals(elem1, elem2)) x.value sequitur) chain[subchain[0]].joined then
+                (List.find(fun x -> List.forall2(fun elem1 elem2 -> String.Equals(elem1, elem2)) x.value sequitur) chain[subchain[0]].joined).amount <- (List.find(fun x -> List.forall2(fun elem1 elem2 -> String.Equals(elem1, elem2)) x.value sequitur) chain[subchain[0]].joined).amount + 1;
+            else
+                chain[subchain[0]].joined <- {value = sequitur; amount = 1; probability = 0} :: chain[subchain[0]].joined;
+        else
+            chain[subchain[0]] <- {joined = [{value = sequitur; amount = 1; probability = 0}]};
+    for link in chain do
+        link.Value.CalculateProbabilities();
+    chain;
+
+for link in getChain(CollectSubchains(sentences, 2)) do
+    printfn "%s :" link.Key
+    for value in link.Value.joined do
+        printf "\t %s - %.2f\n" (String.concat " " value.value) value.probability;
+    printfn "";
