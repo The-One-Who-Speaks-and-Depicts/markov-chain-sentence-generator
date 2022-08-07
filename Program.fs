32ecf03897ef564d6756b1d8708320a00fb43c77
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2022-08-08 00:57:08 +0300

    feat: csv reading from dir and generation based on csv data

--- Program.fs
+++ Program.fs
@@ -3,9 +3,41 @@
 open System;
 open System.Linq;
 open System.Collections.Generic;
+open System.IO;
+open FSharp.Data;
+open ShellProgressBar;
+
+let dir = "F:\\DB\\mcsg\\russian-troll-tweets";
+
+let AppendItem seqToAppendTo itemToAppend =
+    [ yield! seqToAppendTo
+      yield itemToAppend ]
+
+let rec Last = function
+    | hd :: [] -> hd
+    | hd :: tl -> Last tl
+    | _ -> failwith "Empty list."
+
+let tokenize (sentence: string) =
+    "#START" :: List.ofSeq(sentence.Split(" "))
 
 
-let sentences = [ ["#START"; "I"; "swim"; "."]; ["#START"; "I"; "go"; "swimming"; "."] ] ;
+
+let loadFiles(dir: string) =
+    let totalTicks = DirectoryInfo(dir).GetFiles().Length;
+    let options = new ProgressBarOptions ();
+    options.ProgressCharacter <- '*';
+    options.ProgressBarOnBottom <- true;
+    let bar = new ProgressBar(totalTicks, " files loaded", options);
+    let barProgress = using (bar)
+    (
+        [for file in DirectoryInfo(dir).GetFiles() do
+            bar.Tick();
+            if file.FullName.EndsWith(".csv") then
+                yield! (List.ofSeq ((CsvFile.Load(file.FullName).Rows).Select(fun x -> tokenize x[2])))
+        ]
+    );
+
 
 let SentenceOutput(sentence: list<string>) =
     let mutable output = "";
@@ -16,10 +48,6 @@ let SentenceOutput(sentence: list<string>) =
         | _ -> output <- if String.Equals("", output) then token else String.concat " " [output; token]
     output;
 
-let AppendItem seqToAppendTo itemToAppend =
-    [ yield! seqToAppendTo
-      yield itemToAppend ]
-
 let GetNGram (tokenPos: int, n: int, sentence: list<string>) =
     let mutable output : list<string> = [];
     for i = tokenPos to tokenPos + n - 1 do
@@ -28,11 +56,20 @@ let GetNGram (tokenPos: int, n: int, sentence: list<string>) =
 
 
 let CollectSubchains (sentences: list<list<string>>) =
-    let mutable output : list<list<string>> = [];
-    for sentence in sentences do
-        for i = 0 to sentence.Length - 1 do
-            output <- AppendItem output (GetNGram(i, 2, sentence));
-    output;
+    let totalTicks = sentences.Length;
+    let options = new ProgressBarOptions ();
+    options.ProgressCharacter <- '*';
+    options.ProgressBarOnBottom <- true;
+    let bar = new ProgressBar(totalTicks, " sentences preprocessed", options);
+    let barProgress = using (bar)
+    (
+        [for sentence in sentences do
+            bar.Tick();
+            yield! [for i = 0 to sentence.Length do
+                    yield GetNGram(i, 2, sentence)
+            ]
+        ]
+    );
 
 type Chained =
     {value: list<string>;
@@ -58,36 +95,56 @@ type Link =
     member this.ExistingTail (tail : list<string>) = List.tryFind(fun x -> List.forall2(fun elem1 elem2 -> String.Equals(elem1, elem2)) x.value tail) this.joined;
 
     member this.ReturnRandomWord() =
-        let random = Random().NextDouble();
-        let tails = List.map (fun x -> {value = x.value; probability = abs x.probability - random; amount = x.amount}) this.joined;
-        tails.Where(fun x -> x.probability.Equals(tails.Select(fun x -> x.probability).Max())).First().value;
+        let random = Random().Next(0, this.joined.Length);
+        this.joined[random].value;
+
+let GetFullChain(collectedSubchains: list<list<string>>) =
+    let totalTicks = collectedSubchains.Length;
+    let options = new ProgressBarOptions ();
+    options.ProgressCharacter <- '*';
+    options.ProgressBarOnBottom <- true;
+    let bar = new ProgressBar(totalTicks, " subchains preprocessed", options);
+    let flow = using (bar)
+    (
+        [for subchain in collectedSubchains do
+            bar.Tick();
+            yield KeyValuePair.Create(subchain.Head, subchain.Tail);
+        ];
+    )
+
+type Chain =
+    {mutable links: list<KeyValuePair<string, Link>>;}
+
+    member this.CalculateProbabilitiesForChain() =
+        for link in this.links do
+            link.Value.CalculateProbabilities();
+
+let GetChain (fullChain: list<KeyValuePair<string, list<string>>>) =
+    let groupedPairs = fullChain.GroupBy(fun x -> x.Key);
+    let totalTicks = groupedPairs.Count();
+    let options = new ProgressBarOptions ();
+    options.ProgressCharacter <- '*';
+    options.ProgressBarOnBottom <- true;
+    let bar = new ProgressBar(totalTicks, " keys preprocessed", options);
+    let flow = using (bar)
+    (
+        let chain = {links = [for group in groupedPairs do
+                                bar.Tick();
+                                let subgroup = group.GroupBy(fun x -> x.Value);
+                                yield KeyValuePair.Create(group.Key, {joined = [for infragroup in subgroup do
+                                                                                    yield {value = infragroup.Key; amount = infragroup.Count(); probability = 0}
+                                ]})
+        ]}
+        chain.CalculateProbabilitiesForChain();
+        chain;
+
+    )
 
 
-
-let GetChain(collectedSubchains: list<list<string>>) =
-    let mutable chain  = new Dictionary<string, Link>();
-    for subchain in collectedSubchains do
-        if (List.tryFind(fun x -> String.Equals(x, subchain.Head)) (List.ofSeq(chain.Keys))).IsSome then
-            let listWithTail = chain[subchain.Head].ExistingTail(subchain.Tail);
-            if listWithTail.IsSome then
-                listWithTail.Value.IncrementAmount();
-            else
-                chain[subchain.Head].joined <- {value = subchain.Tail; amount = 1; probability = 0} :: chain[subchain.Head].joined;
-        else
-            chain[subchain.Head] <- {joined = [{value = subchain.Tail; amount = 1; probability = 0}]};
-    for link in chain do
-        link.Value.CalculateProbabilities();
-    chain;
-
-let rec Last = function
-    | hd :: [] -> hd
-    | hd :: tl -> Last tl
-    | _ -> failwith "Empty list."
-
-let Generate(chain : Dictionary<string, Link>) =
+let Generate(chain : Chain) =
     let mutable finalSentence = ["#START"];
-    while not (String.Equals (Last finalSentence, "#END")) do
-        finalSentence <- finalSentence @ (chain[Last finalSentence].ReturnRandomWord());
+    while ((not (String.Equals (Last finalSentence, "#END"))) && ([for i in finalSentence do i.Length].Sum() < 286)) do
+        finalSentence <- finalSentence @ (chain.links.Where(fun x -> x.Key = Last finalSentence).First().Value.ReturnRandomWord());
     finalSentence;
 
-printfn "%s" (sentences |> CollectSubchains |> GetChain |> Generate |> SentenceOutput);
+printfn "%s" (dir |> loadFiles |> CollectSubchains |> GetFullChain |> GetChain |> Generate |> SentenceOutput);
